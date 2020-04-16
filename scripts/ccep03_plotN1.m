374b348fd0ad5cc2e83da953de702a4335ca3c3e
DoraHermes <hermes.dora@mayo.edu>
2020-04-15 23:09:41 -0500

    nicer way to get from-to stim

--- scripts/ccep03_plotN1.m
+++ scripts/ccep03_plotN1.m
@@ -14,67 +14,58 @@ roi(3).areas = {'25','26','27'};
 % G_occipital_middle G_oc-temp_med-Lingual Pole_occipital
 roi(4).areas = {'19','22','42'};
 
+%% add: load the n1Latencies from the derivatives
+
+
 %% frontal to temporal connections
 
 %%% need to wrap this in a function where we can enter 2 areas of interest
 
+roi_start = roi(1).areas;
+roi_end = roi(3).areas;
+
 out = [];
 
 for kk = 1:length(n1Latencies) % loop subjects
-    out(1).sub(kk).age = n1Latencies(kk).age;
-    out(2).sub(kk).age = n1Latencies(kk).age;
+    out.sub(kk).age = n1Latencies(kk).age;
     
     % initialize connections as empty
-    out(1).sub(kk).latencies = []; % temporal --> frontal
-    out(2).sub(kk).latencies = []; % frontal --> temporal
+    out.sub(kk).samples = []; % roi_start --> roi_end
+    out.sub(kk).latencies = []; % roi_start --> roi_end
     
     for ll = 1:length(n1Latencies(kk).run) % loop runs       
         % run through all first stimulated channels (nr 1 in pair)
-        for ch = 1:size(n1Latencies(kk).run(ll).n1_peak_sample,2)
+        for chPair = 1:size(n1Latencies(kk).run(ll).n1_peak_sample,2)
             
             % temporal --> frontal
-            % if the stimulated channel 1 is temporal
-            if ismember(n1Latencies(kk).run(ll).average_ccep_DestrieuxNr{ch,1},roi(1).areas)
+            % if stimulated channel 1 or 2 is on roi_start
+            if ismember(n1Latencies(kk).run(ll).average_ccep_DestrieuxNr{chPair,1},roi_start) || ...
+                    ismember(n1Latencies(kk).run(ll).average_ccep_DestrieuxNr{chPair,2},roi_start)
                 % run through all measured channels
                 for chSig = 1:size(n1Latencies(kk).run(ll).n1_peak_sample,1)
                     thisChanDestrieuxNr = n1Latencies(kk).run(ll).channel_DestrieuxNr{chSig};
-                    if ~isempty(thisChanDestrieuxNr)
-                        % if a frontal channel is measured
-                        if ismember(thisChanDestrieuxNr,roi(2).areas)
-                            % we have a temporal stim, frontal measured pair
-                            thisSample = n1Latencies(kk).run(ll).n1_peak_sample(chSig,ch);
+                    if isnan(thisChanDestrieuxNr)
+                        thisChanDestrieuxNr = '';
+                    end
+                    if ~isempty(thisChanDestrieuxNr) 
+                        % if roi_end channel is measured
+                        if ismember(thisChanDestrieuxNr,roi_end)
+                            % we have a roi_start stim, roi_end measured pair
+                            thisSample = n1Latencies(kk).run(ll).n1_peak_sample(chSig,chPair);
                             
                             %%% this is still in samples, need to put this
                             %%% in time
-                            out(1).sub(kk).latencies = [out(1).sub(kk).latencies thisSample];
+                            out.sub(kk).samples = [out.sub(kk).samples thisSample];
+                            if ~isnan(thisSample)
+                                out.sub(kk).latencies = [out.sub(kk).latencies n1Latencies(kk).run(ll).tt(thisSample)];
+                            else
+                                out.sub(kk).latencies = [out.sub(kk).latencies NaN];
+                            end
                         end
                     end
                 end
             end
-            
-            %%%% add here also if stimulated channel 2 is temporal!
-            
-            % frontal --> temporal
-            % if the stimulated channel 1 is frontal
-            if ismember(n1Latencies(kk).run(ll).average_ccep_DestrieuxNr{ch,1},roi(2).areas)
-                % run through all measured channels
-                for chSig = 1:size(n1Latencies(kk).run(ll).n1_peak_sample,1)
-                    thisChanDestrieuxNr = n1Latencies(kk).run(ll).channel_DestrieuxNr{chSig};
-                    if ~isempty(thisChanDestrieuxNr)
-                        % if a frontal channel is measured
-                        if ismember(thisChanDestrieuxNr,roi(1).areas)
-                            % we have a temporal stim, frontal measured pair
-                            thisSample = n1Latencies(kk).run(ll).n1_peak_sample(chSig,ch);
-                            
-                            %%% this is still in samples, need to put this
-                            %%% in time
-                            out(2).sub(kk).latencies = [out(2).sub(kk).latencies thisSample];
-                        end
-                    end
-                end
-            end
-            
-            %%%% add here also if stimulated channel 2 is frontal!
+                       
         end
         
     end
@@ -82,7 +73,7 @@ end
 
 %% figure
 
-outInd = 2;
+outInd = 1;
 
 % initialize output: age, mean and variance in latency per subject
 my_output = NaN(length(out(outInd).sub),3);
@@ -96,30 +87,16 @@ end
 
 figure
 subplot(2,1,1),hold on
-plot(my_output(:,1),my_output(:,2),'.')
+plot(my_output(:,1),1000*my_output(:,2),'b.')
 xlabel('age (years)'),ylabel('mean latency (ms)')
-[r,p] = corr(my_output(~isnan(my_output(:,2)),1),my_output(~isnan(my_output(:,2)),2),'Type','Spearman');
+[r,p] = corr(my_output(~isnan(my_output(:,2)),1),my_output(~isnan(my_output(:,2)),2),'Type','Pearson');
 title(['r=' num2str(r,3) ' p=' num2str(p,3)])
 
 subplot(2,1,2),hold on
-plot(my_output(:,1),my_output(:,3),'.')
+plot(my_output(:,1),my_output(:,3),'r.')
 xlabel('age (years)'),ylabel('variance in latency')
-[r,p] = corr(my_output(~isnan(my_output(:,3)),1),my_output(~isnan(my_output(:,3)),3),'Type','Spearman');
+[r,p] = corr(my_output(~isnan(my_output(:,3)),1),my_output(~isnan(my_output(:,3)),3),'Type','Pearson');
 title(['r=' num2str(r,3) ' p=' num2str(p,3)])
 
-
-
-
-%%
-
-%                    allLatencies: [1×411 double]
-%                  n1_peak_sample: [133×46 double]
-%                   channel_names: {133×1 cell}
-%              average_ccep_names: {46×1 cell}
-%                   good_channels: [104×1 double]
-%     average_ccep_DestrieuxLabel: {46×2 cell}
-%        average_ccep_DestrieuxNr: {46×2 cell}
-%          channel_DestrieuxLabel: {133×1 cell}
-%             channel_DestrieuxNr: {133×1 cell}
 
 
