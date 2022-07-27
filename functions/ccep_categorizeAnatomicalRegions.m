4c8c427c13004fa93c32cc9aae1f80bb38fa28ea
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2022-07-27 18:21:07 -0500

    Updated the tract and end-point ROIs definitions file with a more fitting structure

--- functions/ccep_categorizeAnatomicalRegions.m
+++ functions/ccep_categorizeAnatomicalRegions.m
@@ -1,6 +1,5 @@
-function [roi_track,roi_name,roi] = ccep_categorizeAnatomicalRegions()
 %
-% function [roi_track,roi_name,roi] = ccep_categorizeAnatomicalRegions()
+% [roi_track, roi_name, roi] = ccep_categorizeAnatomicalRegions()
 % 
 % Here we define the areas (using one or more Destrieux labels from the _electrodes.tsv) that will be used as 
 % "end-point" areas given specific tract(-segments) and areas from the Yeh HCP YA842 tractography atlas.
@@ -9,89 +8,66 @@ function [roi_track,roi_name,roi] = ccep_categorizeAnatomicalRegions()
 % Also see: Yeh, F. C., Panesar, S., Fernandes, D., Meola, A., Yoshino, M., Fernandez-Miranda, J. C., ... & Verstynen, T. (2018). Population-averaged atlas of the macroscale human structural connectome and its network topology. Neuroimage, 178, 57-68.
 %
 % DH, MvdB, DvB, 2022
+%
+function [rois] = ccep_categorizeAnatomicalRegions()
 
-%%
-%  SLF tract-segments
+    %  SLF tract-segments
+    rois(1).tract_name = 'SLF';
+    
+    rois(1).sub_tract(1).name       = 'Frontal-Parietal';
+    rois(1).sub_tract(1).roi1       = [15, 53];               % Yeh: Fmid
+    rois(1).sub_tract(1).roi2       = [25, 26, 27];           % Yeh: Pinf, PSp, Ag
+    rois(1).sub_tract(1).interHemi  = 0;
+    
+    rois(1).sub_tract(2).name       = 'Frontal-Central';
+    rois(1).sub_tract(2).roi1       = [12, 13, 14, 15, 16, 52, 53, 54];     % Yeh: FSp, Finf, Fmid
+    rois(1).sub_tract(2).roi2       = [3, 4, 29, 68, 69];     % Yeh: PreC, ParaC
+    rois(1).sub_tract(2).interHemi  = 0;
+    
+    %  Arcuate Fasciculus tract-segments
+    
+    rois(2).tract_name = 'AF';
+    
+    rois(2).sub_tract(1).name       = 'Frontal-Temporal';
+    rois(2).sub_tract(1).roi1       = [12, 13, 14, 15, 52, 53];    % Yeh: Finf, Fmid
+    rois(2).sub_tract(1).roi2       = [34, 36, 37, 38, 72, 73];    % Yeh: Tinf, TMd, TSp
+    rois(2).sub_tract(1).interHemi  = 0;
 
-
-% Frontal <----> Parietal
-
-% Yeh: Fmid
-roi_track{1} = 'SLF';
-roi_name{1} = 'SLF_FP_front';
-roi{1} = {'15', '53'};
-
-% Yeh: Pinf, PSp, Ag
-roi_track{2} = 'SLF';
-roi_name{2} = 'SLF_FP_parietal';
-roi{2} = {'25', '26', '27'};
-
-
-% Frontal <----> Central
-
-% Yeh: FSp, Finf, Fmid
-roi_track{3} = 'SLF';
-roi_name{3} = 'SLF_FC_frontal';
-roi{3} = {'12', '13', '14', '15', '16', '52', '53', '54'};
-
-% Yeh: PreC, ParaC
-roi_track{4} = 'SLF';
-roi_name{4} = 'SLF_FC_central';
-roi{4} = {'3', '4', '29', '68', '69'};
-
-
-
-%%
-%  Arcuate Fasciculus tract-segments
-
-
-% Frontal <----> Temporal
-
-% Yeh: Finf, Fmid
-roi_track{5} = 'AF';
-roi_name{5} = 'AF_FT_front';
-roi{5} = {'12', '13', '14', '15', '52', '53'};
-
-% Yeh: Tinf, TMd, TSp
-roi_track{6} = 'AF';
-roi_name{6} = 'AF_FT_temp';
-roi{6} = {'34', '36', '37', '38', '72', '73'};
+    
 
 
 
 
+    %{
 
+    %%
+    %  Regions on initial article submission
 
+    % temporal areas:
+    % G_temporal_inf, G_temporal_middle, G_temp_sup-Lateral,
+    % G_oc-temp_med-Parahip, G_oc-temp_lat-fusifor
+    roi{1} = {'37','38','34','23','21'};
+    roi_name{1} = 'temporal';
+    roi_temporal = [37 38 34 23 21];
 
+    % central areas
+    % G_postcentral G_precentral S_central
+    roi{2} = {'28','29','45'};
+    roi_name{2} = 'central';
+    roi_central = [28 29 45];
 
-%{
+    % parietal areas:
+    % G_pariet_inf-Angular, G_pariet_inf-Supramar, G_parietal_sup
+    roi{3} = {'25','26','27'};
+    roi_name{3} = 'parietal';
+    roi_parietal = [25 26 27];
 
-%%
-%  Regions on initial article submission
+    % frontal areas:
+    % G_front_inf-Triangul, G_front_middle, G_front_inf-Opercular
+    roi{4} = {'14','15','12'}; 
+    roi_name{4} = 'frontal';
+    roi_frontal = [14 15 12]; 
 
-% temporal areas:
-% G_temporal_inf, G_temporal_middle, G_temp_sup-Lateral,
-% G_oc-temp_med-Parahip, G_oc-temp_lat-fusifor
-roi{1} = {'37','38','34','23','21'};
-roi_name{1} = 'temporal';
-roi_temporal = [37 38 34 23 21];
-
-% central areas
-% G_postcentral G_precentral S_central
-roi{2} = {'28','29','45'};
-roi_name{2} = 'central';
-roi_central = [28 29 45];
-
-% parietal areas:
-% G_pariet_inf-Angular, G_pariet_inf-Supramar, G_parietal_sup
-roi{3} = {'25','26','27'};
-roi_name{3} = 'parietal';
-roi_parietal = [25 26 27];
-
-% frontal areas:
-% G_front_inf-Triangul, G_front_middle, G_front_inf-Opercular
-roi{4} = {'14','15','12'}; 
-roi_name{4} = 'frontal';
-roi_frontal = [14 15 12]; 
-
-%}
+    %}
+    
+end
