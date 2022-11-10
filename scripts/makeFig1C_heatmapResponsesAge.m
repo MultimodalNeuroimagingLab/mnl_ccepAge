858b869d8c934fa1c2c6391d0338bff9db088f3d
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2022-11-10 11:55:39 -0600

    Split Fig1 A and B scripts

--- scripts/makeFig1C_heatmapResponsesAge.m
+++ scripts/makeFig1C_heatmapResponsesAge.m
@@ -1,7 +1,7 @@
 % 
 % 
-%  This code is used to plot Fig 1C - the normalized ccep of all patients in order of age, 
-%  and Fig 1B - example CCEPs for a few older and younger subjects 
+%  This code is used to create Figure 1C - the normalized CCEPs of all patients in order of age, 
+%  
 %
 
 
@@ -16,19 +16,11 @@ warning('backtrace', 'off')
 myDataPath = setLocalDataPath(1);
 track_path = fullfile(myDataPath.input, 'sourcedata', 'tracks');
 
-if exist(fullfile(myDataPath.output, 'derivatives', 'av_ccep', 'ccepData_V2.mat'), 'file')
-    load(fullfile(myDataPath.output, 'derivatives', 'av_ccep', 'ccepData_V2.mat'), 'ccepData')
-else
-    disp('Run scripts ccep02_aggregateToStruct.m and ccep03_addtracts.m first')
-end
-
 if exist(fullfile(myDataPath.output, 'derivatives', 'av_ccep', 'ccepAverages.mat'), 'file')
     load(fullfile(myDataPath.output, 'derivatives', 'av_ccep', 'ccepAverages.mat'))
 else
     disp('Run script ccep04_averageConnections.m first')
 end
-
-
 
 
 
@@ -132,71 +124,4 @@ for iTr = 1:length(rois)
 end
 
 
-
-%%
-% Figure 1B - Plot example CCEPs for a few older and younger subjects 
-%
-
-ttmin = -0.300;
-ttmax = .400;
-
-for iTr = 3                 % SLF
-    for iSubTr = 1          % parietal->frontal/frontal->parietal subtract
-        for iDir = [true]   % parietal to frontal
-            
-            % construct sub-tract string
-            subDir = split(rois(iTr).sub_tract(iSubTr).name, '-');
-            strSubTitle = [subDir{iDir + 1}, ' -> ', subDir{~iDir + 1}];
-            
-            if ~isempty(sortedCCEPs{iTr}{iSubTr}{iDir + 1}.age)
-                
-                %
-                figure('Position', [0 0 500 150])
-                plot(1000 * tt(tt > ttmin & tt < ttmax), ...
-                        zeros(size(tt(tt > ttmin & tt < ttmax))),'Color',[.5 .5 .5]);
-                hold on
-                
-                % plot traces for a couple of young subjects
-                sortedCCEPs{iTr}{iSubTr}{iDir + 1}.age([1 4 5])
-                for iSubj = [1 4 5]
-                    plot(1000 * tt(tt > ttmin & tt < ttmax), ...
-                            sortedCCEPs{iTr}{iSubTr}{iDir + 1}.averageResp_nonnorm(iSubj, tt > ttmin & tt < ttmax), 'k', 'LineWidth', 1);
-                end
-                
-                % plot traces for a couple of older subjects
-                sortedCCEPs{iTr}{iSubTr}{iDir + 1}.age([28 30 32])
-                for iSubj = [28 30 32]
-                    plot(1000 * tt(tt > ttmin & tt < ttmax), ...
-                            sortedCCEPs{iTr}{iSubTr}{iDir + 1}.averageResp_nonnorm(iSubj, tt > ttmin & tt < ttmax), 'b', 'LineWidth', 1);
-                end
-
-                hold on
-                set(gca, 'XTick', 0:50:100, 'YTick', [-250 0 250])
-                axis tight
-                ylim([-350 150])
-
-                fill([-10 10 10 -10], [500 500 -500 -500], [.5 .5 .5], 'EdgeColor', 'w', 'FaceAlpha', .8)
-
-                strSign = [' (p\_fdr = ', num2str(all_p_fdr{iTr}{iSubTr}(iDir + 1)), ')'];
-                if all_p_fdr{iTr}{iSubTr}(iDir + 1) < .05,  strSign = [strSign, ' *'];     end
-                title([rois(iTr).tract_name, ' - ', strSubTitle, strSign]);
-
-                %
-                % save
-                %
-                if ~exist(fullfile(myDataPath.output,'derivatives', 'age'), 'dir')
-                    mkdir(fullfile(myDataPath.output,'derivatives', 'age'));
-                end
-                figureName = fullfile(myDataPath.output,'derivatives', 'age', ['CCEPexamples_6subjects', '_', rois(iTr).tract_name, '_', strrep(strSubTitle, ' -> ', '_')]);
-
-                set(gcf,'renderer','Painters')
-                set(gcf,'PaperPositionMode','auto')
-                print('-dpng','-r300',figureName)
-                print('-depsc','-r300',figureName)
-                close(gcf)
-                
-            end
-        end
-    end
-end
 
